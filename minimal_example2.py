2e8387574a7ba2a0ca0c3880bcbcfc9b117c90c9
Adrián Javaloy <adrian.javaloy@gmail.com>
2021-02-09 16:28:06 +0100

    updated minimal_example2

--- minimal_example2.py
+++ minimal_example2.py
@@ -1,26 +1,38 @@
-from lipstd.scaler import LipschitzScaler
-from lipstd.likelihoods import *
+import torch
+import torch.nn as nn
+from lipstd import *
 
 # Declare likelihood
 likelihood = get_likelihood('gamma', 'normal', 'categorical(3)', 'categorical(2)*', 'bernoulli*', 'poisson*')
+print('Likelihoods:', likelihood, len(likelihood))
+likelihood.eval()
+print('Likelihoods:', likelihood, len(likelihood))
+likelihood.train()
+
+# For MNIST you could do something like this
+# likelihood = get_likelihood({'name': 'bernoulli*', 'domain_size': 784})
 
 # Create some synthetic data
 params = torch.randn([likelihood.num_params])
-for i in [0, 7, 9, 11, 13]:
+# for i in [0, 7, 9, 11, 13]:
+for i in [0, 9, 11]:
     params[i] += 1  # to ensure that the Gammas have at least mode 1
 
 likelihood.eval()
 data = likelihood(*params).sample([1000])
 likelihood.train()
 
-# Apply Lipschitz standardization
-scaler = LipschitzScaler(likelihood, 1 / 0.001)
-scaler.fit(data)
+print('='*16)
+for i in likelihood(*params):
+    print(i)
+print('=' * 16)
 
-# For MNIST you could do something like this
-# likelihood = LikelihoodFlatten(GammaTrick(Bernoulli(domain_size=784)))
-# scaler = LipschitzScaler(likelihood, 1. / args.learning_rate)
-# scaler.fit(data_loader.dataset.data.flatten(start_dim=1) / 255.)
+# Apply Lipschitz standardization
+scaler = LipschitzScaler(likelihood, 1 / 0.001, verbose=True)
+# scaler = StandardScaler(likelihood, verbose=True)
+# scaler = NormalizationScaler(likelihood, verbose=True)
+# scaler = InterquartileScaler(likelihood, verbose=True)
+scaler.fit(data)
 
 print("Likelihood: 'gamma', 'normal', 'categorical(3)', 'categorical(2)*', 'bernoulli*', 'poisson*'")
 pos = 0
@@ -35,6 +47,10 @@ net = nn.Sequential(
     nn.Linear(likelihood.domain_size, 100), nn.Tanh(),
     nn.Linear(100, likelihood.num_params)
 )
+
+params = net(data)
+p_x = likelihood(*params.unbind(dim=-1))
+print('Log evidence:', p_x.log_prob(data).mean(dim=0).tolist())
 
 net.eval()
 
